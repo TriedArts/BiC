836352cbd26be9d0891da755e89e200a4e912d28
TriedArts <70377964+TriedArts@users.noreply.github.com>
2023-01-14 00:26:56 +0000

    First commit

---
+++ src/BiC.java
@@ -0,0 +1,34 @@
+/*
+      ####    ###   ####   #   #    ###  #   #     ###      #    #     #  ####   ####   ###  ####    ###   ####
+      #   #  #   #  #   #  ##  #     #   ##  #    #   #    # #   ##   ##  #   #  #   #   #   #   #  #   #  #
+      #   #  #   #  #   #  ##  #     #   ##  #    #        # #   # # # #  #   #  #   #   #   #   #  #      #
+      ####   #   #  ####   # # #     #   # # #    #       #   #  #  #  #  ####   ####    #   #   #  # ###  ###
+      #   #  #   #  #   #  #  ##     #   #  ##    #       #####  #     #  #   #  #   #   #   #   #  #   #  #
+      #   #  #   #  #   #  #  ##     #   #  ##    #   #  #     # #     #  #   #  #   #   #   #   #  #   #  #
+      ####    ###   #   #  #   #    ###  #   #     ###   #     # #     #  ####   #   #  ###  ####    ###   ####
+ */
+// I M P O R T S
+// M A I N  C L A S S  D E F I N I T I O N
+/**
+ * BiC - Born in Cambridge
+ *
+ * The birthplace of modern football. 1848 the first draft of rules for football as we know it today was written down in Cambridge, UK.
+ *
+ * @author Patrick Ebert
+ * @version 0.01
+ *
+ */
+public class BiC
+{
+    // S T A T I C  C O N S T A N T S
+    public static final String author  = "Patrick Ebert";
+    public static final String version = "0.01";
+
+    // M A I N  F U N C T I O N
+    public static void main(String [] args)
+    {
+        System.out.println("Born in Cambridge");
+        System.out.println("A football manager by " + BiC.author);
+        System.out.println("version " + BiC.version);
+    }
+}
